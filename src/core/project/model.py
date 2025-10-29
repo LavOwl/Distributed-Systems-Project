ac467ac73c5d87371ec593e8202f0c98d878c40e
Arkimss <acostasalim54@gmail.com>
2025-10-29 14:09:20 +0000

    Agrego modelo de observaciones y modifico el modelo de los proyectos y las etapas

--- src/core/project/model.py
+++ src/core/project/model.py
@@ -2,13 +2,13 @@ from src.core.database import db
 from sqlalchemy import Enum
 import enum
 
-class ProjectStatus(enum.Enum):
-    """
-    Enum para los estados de un proyecto.
-    """
-    PENDIENTE = "Pendiente"
-    EN_PROGRESO = "En progreso"
-    COMPLETADO = "Completado"
+# class ProjectStatus(enum.Enum):
+#     """
+#     Enum para los estados de un proyecto.
+#     """
+#     PENDIENTE = "Pendiente"
+#     EN_PROGRESO = "En progreso"
+#     COMPLETADO = "Completado"
 
 
 class Project(db.Model):
@@ -20,7 +20,7 @@ class Project(db.Model):
     case_id = db.Column(db.Integer, nullable=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255), nullable=True)
-    status = db.Column(db.Enum(ProjectStatus), default=ProjectStatus.PENDIENTE, nullable=False)
+    #status = db.Column(db.Enum(ProjectStatus), default=ProjectStatus.PENDIENTE, nullable=False)
 
     # Relación entre project y stage.
     stages = db.relationship("Stage", back_populates="project")
--- src/core/stage/model.py
+++ src/core/stage/model.py
@@ -10,21 +10,37 @@ class CoverageRequest(enum.Enum):
     MATERIALES = "MATERIALES"
     MANO_DE_OBRA = "MANO_DE_OBRA"
 
+class StatusStage(enum.Enum):
+    """
+    Enum para los estados de una etapa del proyecto.
+    """
+    PENDING = "PENDING"
+    IN_PROGRESS = "IN_PROGRESS"
+    FINISHED = "FINISHED"
 
 class Stage(db.Model):
     """
     Modelo para representar la etapa de un proyecto.
     """
-    __tablename__ = "stage"
+    __tablename__ = "stages"
     id = db.Column(db.Integer, primary_key=True)
+    id_project = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=True)
     coverage_request = db.Column(Enum(CoverageRequest), nullable=False)
-    requires_contributor = db.Column(db.Boolean, nullable=False, default=False)
-
-    # Clave foránea para acceder al proyecto.
-    project_id = db.Column(db.Integer, db.ForeignKey("project.id"), nullable=False)
-
-    # Relación entre project y stage.
-    project = db.relationship("Project", back_populates="stages")
+    status = db.Column(Enum(StatusStage), default=StatusStage.PENDING, nullable=False)
+    
+    
+    def to_dict(self):
+           return {
+                "id": self.id,
+                "id_project": self.id_project,
+                "name": self.name,
+                "description": self.description,
+                "start_date": self.start_date.isoformat() if self.start_date else None,
+                "end_date": self.end_date.isoformat() if self.end_date else None,
+                "coverage_request": self.coverage_request.name if self.coverage_request else None,
+                "status": self.status.name if self.status else None
+            }
