9cbe3202c99d670253d4f5cd2fef1f481c8d3c90
Marcos Gerez <marcosmateogerez@gmail.com>
2025-11-05 18:42:37 -0300

    Actualización controlador login.

--- src/web/controllers/bonita_controller.py
+++ src/web/controllers/bonita_controller.py
@@ -7,6 +7,37 @@ import os
 bonita_bp = Blueprint("bonita", __name__)
 BONITA_BASE_URL = os.environ.get("BONITA_BASE_URL")
 
+@bonita_bp.post("/v1/login")
+def bonita_login():
+    """
+    Realiza el login en Bonita y devuelve un mensaje de éxito o error.
+    (1) Recibe (JSON en el BODY):
+        1. username: string.
+        2. password: string.
+    (2) Devuelve:
+        1. 401 - No se pudo autenticar en Bonita.
+        2. 200 - Login exitoso.
+    """
+    # Recibe el JSON del body, y hace el login con Bonita.
+    data = request.get_json(silent=True) or {}
+    bonita = BonitaService()
+    session = bonita.bonita_login(data)
+
+    # Verifica si se puede autenticar correctamente.
+    if not session:
+            return jsonify({"error": "No se pudo autenticar en Bonita"}), 401
+    
+    # Obtiene las cookies desde la sesión.
+    jsessionid = session.cookies.get("JSESSIONID")
+    token = session.cookies.get("X-Bonita-API-Token")
+
+    # Devolverlas al cliente (por header o cookie HTTP)
+    response = jsonify({"message": "Login exitoso"})
+    response.set_cookie("JSESSIONID", jsessionid)
+    response.set_cookie("X-Bonita-API-Token", token)
+    return response, 200
+
+
 @bonita_bp.post("/v1/iniciar_proyecto")
 def iniciar_proyecto():
     """
@@ -41,19 +72,3 @@ def iniciar_proyecto():
         }), 201
     except Exception as e:
         return jsonify({"error": f"Error en Bonita: {str(e)}"}), 500
-
-
-@bonita_bp.post("/v1/login")
-def bonita_login():
-    """
-    Realiza el login en Bonita y devuelve un mensaje de éxito o error.
-    """
-    # Recibe el JSON del body.
-    data = request.get_json(silent=True) or {}
-    
-    # Realiza el login con Bonita.
-    bonita = BonitaService()
-    if bonita.bonita_login(data):
-        return jsonify({"message": "Login en Bonita exitoso."}), 201
-    else:
-        return jsonify({"error": "No se pudo autenticar en Bonita"}), 500
